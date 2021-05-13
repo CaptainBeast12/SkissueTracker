2beeda29e19bce9009b6f872053157143f384980
CaptainBeast12 <81479383+CaptainBeast12@users.noreply.github.com>
2021-05-13 21:50:15 +0600

    Update counter command
    The counter is now displayed in a nice embed message.

--- commands/counter.js
+++ commands/counter.js
@@ -3,7 +3,19 @@ module.exports = {
 	description: 'Shows how many times a certain someone has said skill issue.',
 	async execute(message, args) {
 		const counter = require('../counter.json');
-        const user = await message.guild.members.fetch('775377251859824651');
-        message.channel.send(`${user.displayName} has said skill issue ${counter.SkillIssue[user.id]} time(s)!`);
+		let description = '';
+        for (const key in counter.SkillIssue) {
+			const user = await message.guild.members.fetch(key);
+			if (!user) continue;
+			description += `**${user.displayName}** has said skill issue ${counter.SkillIssue[key]} amount of time(s)!\n`;
+		}
+
+		if (description.length <= 0) return;
+		const counterEmbed = {
+			color: 0x2f78d6,
+			title: 'Skill issue counter',
+			description: description
+		};
+		message.channel.send({embed: counterEmbed});
 	},
 };
