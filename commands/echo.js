9db0f690a2066c2244444202ca95b50279f93009
CaptainBeast12 <81479383+CaptainBeast12@users.noreply.github.com>
2021-05-14 20:21:56 +0600

    Update echo commad
    URLs are now discarded when passed in.

--- commands/echo.js
+++ commands/echo.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'echo',
 	description: 'Echos back whatever is passed in the args.',
 	execute(message, args) {
-		for (let i = 0; i < args.length; i++) args[i] = args[i].replace(/@everyone|@here/g, '');
+		for (let i = 0; i < args.length; i++) args[i] = args[i].replace(/@everyone|@here|(^|\s)((https?:\/\/)?[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)/gi, '');
 		const messageToSend = args.join(' ');
 		if (messageToSend === null || messageToSend.match(/^ *$/) !== null) return;
 		message.channel.send(messageToSend);
