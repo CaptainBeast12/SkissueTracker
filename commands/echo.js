3f3e1ae3f24d4a15a4d042a4a881f1a601cd835a
CaptainBeast12 <81479383+CaptainBeast12@users.noreply.github.com>
2021-05-14 15:49:19 +0600

    Update echo.js
    Now uses regular expressions to match and replace everyone and here mentions.

--- commands/echo.js
+++ commands/echo.js
@@ -2,13 +2,7 @@ module.exports = {
 	name: 'echo',
 	description: 'Echos back whatever is passed in the args.',
 	execute(message, args) {
-		for (let i = 0; i < args.length; i++) {
-			if (args[i].includes('@here') || args[i].includes('@everyone')) {
-				args[i] = args[i].replace('@here', '');
-				args[i] = args[i].replace('@everyone', '');
-			}
-		}
-
+		for (let i = 0; i < args.length; i++) args[i] = args[i].replace(/@everyone|@here/g, '');
 		const messageToSend = args.join(' ');
 		if (messageToSend === null || messageToSend.match(/^ *$/) !== null) return;
 		message.channel.send(messageToSend);
